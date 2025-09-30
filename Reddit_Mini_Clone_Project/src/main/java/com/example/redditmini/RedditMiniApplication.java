94e90f9947a67b9281706c60703b9feeb2eaa61c
fzmehrizi <65924618+fzmehrizi@users.noreply.github.com>
2025-09-30 17:46:09 -0400

    First commit for day 2

---
+++ Reddit_Mini_Clone_Project/src/main/java/com/example/redditmini/RedditMiniApplication.java
@@ -0,0 +1,3 @@
+package com.example.redditmini;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class RedditMiniApplication{ public static void main(String[] args){ SpringApplication.run(RedditMiniApplication.class,args);} }
